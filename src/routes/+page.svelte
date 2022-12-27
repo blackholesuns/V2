7f8cddfed06c4017bc9669f212034fbca6f616b8
CEbbinghaus <git@cebbinghaus.com>
2022-12-28 01:11:55 +1100

    Added entry loading to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script>
-	import("$lib/Environment").then(console.log)
+	import Entry from '$component/Entries/Entry.svelte';
+	import { GetLatestEntries } from '$lib/Firebase/Firestore';
+
+	import('$lib/Environment').then(console.log);
+
+	// console.log("Querying Documents")
+	const promise = GetLatestEntries();
 </script>
 
 <svelte:head>
@@ -9,7 +15,21 @@
 
 <div class="mt-12 text-center">
 	<h1 class="text-3xl font-bold underline">NMSCE App V2</h1>
-	<p >The latest Iteration</p>
+	<p>The latest Iteration</p>
+	<a
+		class="text-indigo-600"
+		href="/Entry/c28cfcaf-a8b0-47c0-8a8f-ba3609da332a?galaxy=euclid&type=Ship">Example Entry</a
+	>
 
-	<a class="text-indigo-600" href="/Entry/c28cfcaf-a8b0-47c0-8a8f-ba3609da332a?galaxy=euclid&type=Ship">Example Entry</a>
+	<div class="flex flex-row flex-wrap w-screen">
+		{#await promise}
+			<p>...waiting</p>
+		{:then entries}
+			{#each entries.data as entry}
+				<Entry data={entry} />
+			{/each}
+		{:catch error}
+			<p style="color: red">{error.message}</p>
+		{/await}
+	</div>
 </div>
