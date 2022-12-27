cef146bbb00927940171a405c8fa0bf385cdc787
CEbbinghaus <git@cebbinghaus.com>
2022-12-27 15:20:32 +0000

    improved decontainer. Commented gitignore and disabled ssr

--- src/Routes/Entry/[id]/+page.server.ts
+++ src/Routes/Entry/[id]/+page.server.ts
@@ -2,6 +2,7 @@
 import { error } from '@sveltejs/kit';
 import { GetAPIEntry } from '$lib/Firebase/Api';
 
+export const ssr = false;
 // export const hydrate = false;
 // export const router = false;
 
