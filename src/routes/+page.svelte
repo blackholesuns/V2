106b2f3a172f9cab84bce78e72a8bd9f1e2466f5
CEbbinghaus <git@cebbinghaus.com>
2022-12-27 15:48:06 +1100

    Added Tailwind & fixed Debugging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>NMSCE App V2</h1>
-<p>The latest Iteration</p>
+<div class="mt-12">
+	<h1 class="text-3xl font-bold underline text-center">NMSCE App V2</h1>
+	<p class="text-center">The latest Iteration</p>
+</div>
