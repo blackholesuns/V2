1322b06bb43027540ce8f342ccede636e06bbdfc
CEbbinghaus <git@cebbinghaus.com>
2023-07-20 00:07:52 +1000

    Delete src/Library directory
    Case Sensitive relic of the past

--- src/Library/CDN/Images.ts
+++
@@ -1,7 +0,0 @@
-export const BaseUrl = `https://cdn.nmsce.com`;
-export const ProjectUrl = `${BaseUrl}/nmsce`;
-export const OriginalImageUrl = `${ProjectUrl}/orig`
-
-export function GetOriginalImageUrl(imageName: string): string {
-	return `${OriginalImageUrl}/${imageName}`
-}
