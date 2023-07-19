2cb370b108251b5e90a0c9616a06dad33aac96df
CEbbinghaus <git@cebbinghaus.com>
2023-07-20 00:19:59 +1000

    Added loading of the ships on the home page

--- src/library/Firebase/Firestore.ts
+++ src/library/Firebase/Firestore.ts
@@ -1,23 +1,23 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, getDoc, doc, query, collectionGroup, orderBy, limit, getDocs } from "firebase/firestore";
+import { getFirestore, getDoc, doc, query, orderBy, limit, getDocs, collection } from "firebase/firestore";
 import { Config } from "$lib/Firebase/Config";
 import { CapitalizeFirstLetter } from "$lib/Util";
 
 export const App = initializeApp(Config);
 export const Firestore = getFirestore(App);
 
-export async function GetEntry(type: string, id: string, galaxy = "Euclid") {
+export async function GetEntry(id: string, type?: string, galaxy?: string) {
 
-	galaxy = CapitalizeFirstLetter(galaxy);
-	type = CapitalizeFirstLetter(type);
+	galaxy = galaxy && CapitalizeFirstLetter(galaxy);
+	type = type && CapitalizeFirstLetter(type);
 
-    const res = await getDoc(doc(Firestore, `nmsce/${galaxy}/${type}/${id}`));
+    const res = await getDoc(doc(Firestore, `/nmsceCombined/${id}`));
     return {exists: res.exists(), data: res.data()}
 }
 
 export async function GetLatestEntries(maxCount: number = 50) {
 
-    const res = await getDocs(query(collectionGroup(Firestore, "nmsceCommon"), orderBy("created", "desc"), limit(maxCount)));
+    const res = await getDocs(query(collection(Firestore, "nmsceCombined"), orderBy("created", "desc"), limit(maxCount)));
     return {
 		exists: !res.empty,
 		data: res.docs.map(v => v.data())
