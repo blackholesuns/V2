96a9a83bda822166e097c513dc72a4e5ebb0b297
CEbbinghaus <git@cebbinghaus.com>
2023-07-23 02:38:36 +1000

    Updated Icons to NMSGE variants

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -6,7 +6,7 @@
 
 <div id="header" class="h-[64px] w-full acryllic top-0 left-0 sticky shadow-xl flex flex-row">
 	<a class="aspect-square" href="/">
-		<img class="p-2 h-[64px]" src="/app-logo.png" alt="">
+		<img class="p-1 h-[64px]" src="/logo.svg" alt="">
 	</a>
 
 	<div class="ml-auto p-3 h-[64px] flex flex-row">
